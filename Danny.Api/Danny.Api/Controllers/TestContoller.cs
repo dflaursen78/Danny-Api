3ac18d0a0fa456f98cf952d5e5f8dc45a37ce44e
Danny_Laursen <dflaursen@hotmail.com>
2020-05-21 15:05:58 +0200

    disco

---
+++ Danny.Api/Danny.Api/Controllers/TestContoller.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Danny.Api.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestContoller : Controller
+    {
+        [HttpGet]
+        [Route("apiGetTest")]
+        public IActionResult Get()
+        {
+            return Ok("ferie nu");
+        }
+    }
+}
